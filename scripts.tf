7c3c46317a02202d86cbb5a59ed4ee2209f14bee
Cristian Beraha <cristian@cloudops.works>
2025-07-16 17:25:59 -0300

    Add `source_hash` attribute for Synthetics Canary to ensure resource updates on script changes.

--- scripts.tf
+++ scripts.tf
@@ -161,6 +161,7 @@ resource "aws_s3_object" "script_custom" {
   bucket      = local.s3_location_bucket_name
   key         = local.zip_files[each.key].bucket_key
   source      = try(archive_file.script_custom_node[each.key].output_path, archive_file.script_custom_python[each.key].output_path)
+  source_hash = filesha256(try(archive_file.script_custom_node[each.key].output_path, archive_file.script_custom_python[each.key].output_path))
   tags = {
     synthetic_group_key  = each.value.group.name
     synthetic_canary_key = each.value.canary.name
