ce7476cc9fd0bf2a0c01498b59e00fac6d3cf147
Cristian Beraha <cristian@cloudops.works>
2025-07-16 00:20:55 -0300

    Update `scripts.tf`: Replace `local_file.script_config` with `archive_file.script` for `source` references to align with updated S3 upload logic.

--- scripts.tf
+++ scripts.tf
@@ -64,7 +64,7 @@ resource "aws_s3_object" "script" {
   for_each    = local.synthetics
   bucket      = local.s3_location_bucket_name
   key         = local.zip_files[each.key].bucket_key
-  source      = local_file.script_config[each.key].filename
+  source      = archive_file.script[each.key].output_path
   source_hash = sha256(format("%s-%s", local.hash_content[each.key], local.hash_sources))
   # tags = merge(
   #   local.all_tags,
