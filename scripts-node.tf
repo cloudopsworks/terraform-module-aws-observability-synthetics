088fed07014d5332ddfe1f851163181244dedf41
Cristian Beraha <cristian@cloudops.works>
2025-07-17 09:49:11 -0300

    Refactor Synthetics Canary configuration to separate Node.js and Python handling, optimize request type validation, and introduce tailored resources for script and runtime specificity.

---
+++ scripts-node.tf
@@ -0,0 +1,110 @@
+##
+# (c) 2021-2025
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
+#
+
+locals {
+  zip_files_nodejs = {
+    for key, content in local.synthetics : key => {
+      file_path     = "${path.module}/sources/standard/nodejs/"
+      file_name     = "${key}_config.yaml"
+      bucket_key    = "upload/scripts/${key}.zip"
+      zip_file_path = "${path.module}/scripts/${key}.zip"
+    }
+    if strcontains(try(local.request_scripts_map[content.canary.request_script_ref].runtime_version, content.canary.runtime_version, local.default_runtime_version), "nodejs")
+  }
+  nodejs_synthetics_all = {
+    for key, synth in local.synthetics : key => synth
+    if synth.is_nodejs
+  }
+  nodejs_synthetics_url = {
+    for key, synth in local.synthetics : key => synth
+    if synth.is_nodejs && synth.is_url
+  }
+  nodejs_synthetics_custom = {
+    for key, synth in local.synthetics : key => synth
+    if synth.is_nodejs && synth.is_script
+  }
+}
+
+resource "local_file" "script_config_nodejs" {
+  for_each        = local.nodejs_synthetics_all
+  content         = local.canary_content[each.key]
+  filename        = format("%s%s", local.zip_files_nodejs[each.key].file_path, local.zip_files_nodejs[each.key].file_name)
+  file_permission = "0644"
+}
+
+resource "null_resource" "this_nodejs" {
+  triggers = {
+    hash_sources = local.hash_sources
+  }
+  provisioner "local-exec" {
+    command     = "npm install --prefix ./nodejs --no-save --no-package-lock --omit=dev --target_arch=x64 --target_platform=linux js-yaml"
+    working_dir = "${path.module}/sources/standard"
+  }
+}
+
+resource "archive_file" "script_url_nodejs" {
+  for_each    = local.nodejs_synthetics_url
+  output_path = local.zip_files_nodejs[each.key].zip_file_path
+  type        = "zip"
+  source_dir  = "${path.module}/sources/standard"
+  excludes = [
+    "example*.yaml",
+    "requirements.txt",
+    "python/**/*",
+  ]
+  depends_on = [
+    local_file.script_config_nodejs
+  ]
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.this_nodejs,
+      local_file.script_config_nodejs[each.key].id,
+    ]
+  }
+}
+
+resource "aws_s3_object" "script_url_nodejs" {
+  for_each    = local.nodejs_synthetics_url
+  bucket      = local.s3_location_bucket_name
+  key         = local.zip_files_nodejs[each.key].bucket_key
+  source      = archive_file.script_url_nodejs[each.key].output_path
+  source_hash = sha256(format("%s-%s", local.hash_content[each.key], local.hash_sources))
+  tags = {
+    synthetic_group_key  = each.value.group.name
+    synthetic_canary_key = each.value.canary.name
+  }
+}
+
+resource "local_file" "script_custom_node" {
+  for_each        = local.nodejs_synthetics_custom
+  content         = try(local.request_scripts_map[each.value.canary.request_script_ref].content, each.value.canary.request_script)
+  filename        = "${path.module}/sources/custom/${each.key}/nodejs/node_modules/custom_handler.js"
+  file_permission = "0644"
+}
+
+resource "archive_file" "script_custom_node" {
+  for_each    = local.nodejs_synthetics_custom
+  output_path = local.zip_files_nodejs[each.key].zip_file_path
+  type        = "zip"
+  source_dir  = "${path.module}/sources/custom/${each.key}/"
+  excludes = [
+    "**/example*.yaml",
+    "**/requirements.txt",
+  ]
+  depends_on = [
+    local_file.script_custom_node,
+  ]
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.this_nodejs,
+      local_file.script_custom_node[each.key].id,
+    ]
+  }
+}
+
---
+++ scripts-python.tf
@@ -0,0 +1,137 @@
+##
+# (c) 2021-2025
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
+#
+
+locals {
+  # compute has from ${path.module}/sources
+  hash_sources = upper(sha256(join("", [for file_to_hash in fileset(".", "${path.module}/sources/**") : filesha256(file_to_hash)])))
+  canary_content = {
+    for key, synthetic in local.synthetics : key => yamlencode({
+      requests = synthetic.canary.requests
+    })
+    if upper(try(synthetic.canary.requests_type, "URL")) == "URL"
+  }
+  hash_content = {
+    for key, content in local.canary_content : key => upper(sha256(content))
+  }
+  zip_files_python = {
+    for key, content in local.synthetics : key => {
+      file_path     = "${path.module}/sources/standard/python/"
+      file_name     = "${key}_config.yaml"
+      bucket_key    = "upload/scripts/${key}.zip"
+      zip_file_path = "${path.module}/scripts/${key}.zip"
+    }
+    if content.is_python
+  }
+  python_synthetics_all = {
+    for key, synth in local.synthetics : key => synth
+    if synth.is_python
+  }
+  python_synthetics_url = {
+    for key, synth in local.synthetics : key => synth
+    if synth.is_python && synth.is_url
+  }
+  python_synthetics_custom = {
+    for key, synth in local.synthetics : key => synth
+    if synth.is_python && synth.is_script
+  }
+}
+
+resource "local_file" "script_config_python" {
+  for_each        = local.python_synthetics_all
+  content         = local.canary_content[each.key]
+  filename        = format("%s%s", local.zip_files_python[each.key].file_path, local.zip_files_python[each.key].file_name)
+  file_permission = "0644"
+}
+
+resource "null_resource" "this_python" {
+  triggers = {
+    hash_sources = local.hash_sources
+  }
+  provisioner "local-exec" {
+    command     = "python3 -m pip install -r requirements.txt --target ./python --platform manylinux_2_17_x86_64 --python-version 3.11 --no-deps --upgrade"
+    working_dir = "${path.module}/sources/standard"
+  }
+}
+
+resource "archive_file" "script_url_python" {
+  for_each    = local.python_synthetics_url
+  output_path = local.zip_files_python[each.key].zip_file_path
+  type        = "zip"
+  source_dir  = "${path.module}/sources/standard"
+  excludes = [
+    "example*.yaml",
+    "requirements.txt",
+    "nodejs/**/*",
+  ]
+  depends_on = [
+    local_file.script_config_python
+  ]
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.this_python,
+      local_file.script_config_python[each.key].id,
+    ]
+  }
+}
+
+resource "aws_s3_object" "script_url_python" {
+  for_each    = local.python_synthetics_url
+  bucket      = local.s3_location_bucket_name
+  key         = local.zip_files_python[each.key].bucket_key
+  source      = archive_file.script_url_python[each.key].output_path
+  source_hash = archive_file.script_url_python[each.key].output_sha256
+  tags = {
+    synthetic_group_key  = each.value.group.name
+    synthetic_canary_key = each.value.canary.name
+  }
+}
+
+resource "local_file" "script_custom_python" {
+  for_each        = local.python_synthetics_custom
+  content         = try(local.request_scripts_map[each.value.canary.request_script_ref].content, each.value.canary.request_script)
+  filename        = "${path.module}/sources/custom/${each.key}/python/custom_handler.py"
+  file_permission = "0644"
+}
+
+resource "archive_file" "script_custom_python" {
+  for_each    = local.python_synthetics_custom
+  output_path = local.zip_files_python[each.key].zip_file_path
+  type        = "zip"
+  source_dir  = "${path.module}/sources/custom/${each.key}/"
+  excludes = [
+    "**/example*.yaml",
+    "**/requirements.txt",
+  ]
+  depends_on = [
+    local_file.script_custom_python,
+  ]
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.this_python,
+      local_file.script_custom_python[each.key].id,
+    ]
+  }
+}
+
+# Generic for both Node.js and Python custom scripts
+resource "aws_s3_object" "script_custom" {
+  for_each    = merge(local.python_synthetics_custom, local.nodejs_synthetics_custom)
+  bucket      = local.s3_location_bucket_name
+  key         = try(local.zip_files_nodejs[each.key].bucket_key, local.zip_files_python[each.key].bucket_key)
+  source      = try(archive_file.script_custom_node[each.key].output_path, archive_file.script_custom_python[each.key].output_path)
+  source_hash = try(archive_file.script_custom_node[each.key].output_sha256, archive_file.script_custom_python[each.key].output_sha256)
+  tags = {
+    synthetic_group_key  = each.value.group.name
+    synthetic_canary_key = each.value.canary.name
+  }
+  depends_on = [
+    archive_file.script_custom_node,
+    archive_file.script_custom_python,
+  ]
+}
